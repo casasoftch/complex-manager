ee716d0b35af369391201caa9aaccb09e3f908aa
Jeremias Nater <jeremias.nater@casasoft.ch>
2016-06-27 15:43:27 +0200

    removed invalid attributes

--- assets/templates/filter.php
+++ assets/templates/filter.php
@@ -1,16 +1,16 @@
 <div class="complex-filter-wrapper">
 	<div class="complex-filter">
-		<form method="GET" id="complexFilterForm" action="">
+		<form method="GET" id="complexFilterForm">
 			<?php if ($roomfilters): ?>
 				<div class="filteroption complex-filter-checkboxwrapper filteroption-rooms-checkboxes">
-					<label class="filteroption-label" for="">Zimmer</label>
+					<label class="filteroption-label">Zimmer</label>
 					<?php foreach ($roomfilters as $val): ?>
 						<span class="checkboxoption"><input type="checkbox" name="rooms[]" id="cxmFilterRooms_<?php echo $val ?>" value="<?php echo $val ?>"><label for="cxmFilterRooms_<?php echo $val ?>" class="checkbox-inline">&nbsp;<?php echo $val ?></label></span>	
 					<?php endforeach ?>
 				</div>
 			<?php endif ?>
 			<div class="filteroption complex-filter-checkboxwrapper filteroption-status-checkboxes">
-				<label class="filteroption-label" for="">Status</label>
+				<label class="filteroption-label">Status</label>
 				<span class="checkboxoption"><input type="checkbox" name="status[]" id="cxmFilterStatus_available" value="available"><label for="cxmFilterStatus_available" class="checkbox-inline">Verfügbar</label></span>
 			</div>
 		</form>
