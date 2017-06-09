5de4646801a0944d125e294c9e45ae30c1fdc569
Jens Stalder <jens.stalder@casasoft.ch>
2017-06-09 15:43:04 +0200

    ?

--- features/admin/inquiry_options_page.php
+++ features/admin/inquiry_options_page.php
@@ -4,13 +4,14 @@ namespace casasoft\complexmanager;
 
 class inquiry_options_page extends Feature {
 
+	public $prefix = 'complexmanager_inquiry_';
+
 	public function __construct() {
 		if( function_exists('acf_add_local_field_group') ):
 			acf_add_options_sub_page(array(
 				'title' => 'Anfrage Optionen',
 				'parent' => 'edit.php?post_type=complex_inquiry'
 			));
-
 
 
 
@@ -81,7 +82,93 @@ class inquiry_options_page extends Feature {
 
 		endif;
 
+
+
+		if (isset($_GET['csv-download'])) {
+
+
+			$inquiries = get_posts(array(
+		    'post_type' => 'complex_inquiry',
+		    'posts_per_page'   => -1,
+		    'post_status' => 'any',
+		    'post_parent' => 0,
+		    //'orderby' => 'menu_order',
+		    //'order' => 'asc'
+		  ));
+
+
+			$rows = [];
+			foreach ($inquiries as $inq) {
+	      $row = array();
+
+        $row['ID'] = $inq->ID;
+				$row['Titel'] = $inq->post_title;
+
+
+
+
+
+				$fields = array(
+					'reason',
+					'unit_id',
+					'gender',
+					'message',
+					'email',
+					'locality',
+					'postal_code',
+					'street',
+					'mobile',
+					'phone',
+					'legal_name',
+					'last_name',
+					'first_name'
+				);
+				foreach ($fields as $field) {
+					$key = $this->prefix.$field;
+					$value = get_post_meta( $inq->ID, '_'.$key, true );
+					$row[get_cxm_label(false, $field, 'complex_inquiry')] = str_replace("\r", "", str_replace("\n", '' , nl2br($value)));
+				}
+
+				$key = $this->prefix.'extra_data';
+				$value = get_post_meta( $inq->ID, '_'.$key, true );
+				$extra_content = '';
+				foreach ($value as $key => $value) {
+					$extra_content .= str_replace("'", "'", $key) . ': ' . str_replace("\r", "", str_replace("\n", '' , nl2br($value))) ;  //\n 
+				}
+				$row['Extra'] = $extra_content;
+
+
+
+        $rows[] = $row;
+			}
+
+
+			$fp = fopen(wp_upload_dir() . 'anfragen.csv', 'w');
+			$BOM = "\xEF\xBB\xBF"; // UTF-8 BOM
+			fwrite($export); //, "sep=\t".PHP_EOL
+			fwrite($fp, $BOM);
+			if ($rows) {
+			  fputcsv($fp, array_keys($rows[0]));
+
+			  foreach ($rows as $fields) {
+			      fputcsv($fp, $fields);
+			  }
+			}
+
+		  fclose($fp);
+
+	    header('Content-Type: application/octet-stream');
+	    header("Content-Transfer-Encoding: Binary");
+	    header("Content-disposition: attachment; filename=\"" . 'anfragen.csv' . "\"");
+	    echo file_get_contents(wp_upload_dir() . 'anfragen.csv');
+
+		  die();
+		}
+
+
 	}
+
+
 
 }
 add_action( 'complexmanager_init', array( 'casasoft\complexmanager\inquiry_options_page', 'init' ));
