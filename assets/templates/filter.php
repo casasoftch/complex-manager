ef2f0537e6c25689f8958d919fd1347374975a46
StephanieKessler <stephanie.kessler@casasoft.com>
2021-01-21 10:52:11 +0100

    sort values of custom_1, custom_2, and custom_3 for filter

--- assets/templates/filter.php
+++ assets/templates/filter.php
@@ -107,7 +107,7 @@
 					<?php endif ?>
 				<?php elseif ($filtertype == 'custom_3') : ?>
 					<?php if ($custom_3_filters): ?>
-						<?php asort($custom_3_filters); ?>
+						<?php natsort($custom_3_filters); ?>
 						<div class="filteroption complex-filter-checkboxwrapper filteroption-custom_3-checkboxes">
 							<?php  ?>
 							<label class="filteroption-label"><?php echo $value ?></label>
@@ -118,7 +118,7 @@
 					<?php endif ?>
 				<?php elseif ($filtertype == 'custom_2') : ?>
 					<?php if ($custom_2_filters): ?>
-						<?php asort($custom_2_filters); ?>
+						<?php natsort($custom_2_filters); ?>
 						<div class="filteroption complex-filter-checkboxwrapper filteroption-custom_2-checkboxes">
 							<?php  ?>
 							<label class="filteroption-label"><?php echo $value ?></label>
@@ -129,7 +129,7 @@
 					<?php endif ?>
 				<?php elseif ($filtertype == 'custom_1') : ?>
 					<?php if ($custom_1_filters): ?>
-						<?php asort($custom_1_filters); ?>
+						<?php natsort($custom_1_filters); ?>
 						<div class="filteroption complex-filter-checkboxwrapper filteroption-custom_1-checkboxes">
 							<?php  ?>
 							<label class="filteroption-label"><?php echo $value ?></label>
